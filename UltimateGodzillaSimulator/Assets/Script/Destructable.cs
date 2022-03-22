530bcc2ba7b8e53af0a4070b6aeab2a138e59bc9
mikomoares <mihanoch@gmail.com>
2022-03-22 17:54:18 -0300

    bug fix fdp

---
+++ UltimateGodzillaSimulator/Assets/Script/Destructable.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public bool teste;
+    public GameObject broken, explosion, smoke;
+    // Update is called once per frame
+    void Update()
+    {
+        if (teste){
+            Instantiate(broken, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision other) {
+        print(other.gameObject.tag);
+        if (other.gameObject.tag == "Quebrador"){
+            teste = true;
+            //Instantiate(explosion, other.gameObject.transform.position, Quaternion.identity);
+            //Instantiate(smoke, gameObject.transform.position, Quaternion.identity);
+        }
+    }
+}
